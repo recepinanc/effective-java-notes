d3e560310a4c987c97ead854f7a700ef5da963ed
Recep İnanç <recepinanc@users.noreply.github.com>
2019-06-19 08:00:22 +0300

    Create FlyweightDesignPattern.java

---
+++ Creating and Destroying Objects/Keywords/FlyweightDesignPattern.java
@@ -0,0 +1,81 @@
+class Tree
+{
+    private int x, y;
+    private TreeType treeType;
+
+    public Tree(int x, int y, TreeType treeType)
+    {
+        this.x = x;
+        this.y = y;
+        this.treeType = treeType;
+    }
+}
+
+class TreeType
+{
+    private int numberOfBranches;
+    private String color;
+    private boolean isExported;
+
+    public TreeType(int numberOfBranches, String color, boolean isExported)
+    {
+        this.numberOfBranches = numberOfBranches;
+        this.color = color;
+        this.isExported = isExported;
+    }
+
+    public void loseLeaves()
+    {
+        System.out.println("Dropping my leaves as " + this.toString());
+    }
+
+    public int getNumberOfBranches()
+    {
+        return numberOfBranches;
+    }
+
+    public String getColor()
+    {
+        return color;
+    }
+
+    public boolean isExported()
+    {
+        return isExported;
+    }
+}
+
+class TreeFactory {
+    public static ArrayList<TreeType> treeTypes = new ArrayList<>();
+    public static TreeType getTreeType(int numberOfBranches, String color, boolean isExported) {
+        TreeType treeType = treeTypes.find(numberOfBranches, color, isExported);
+
+        if (treeType == null) {
+            treeType = new TreeType(numberOfBranches, color, isExported);
+            treeTypes.add(treeType);
+        }
+
+        return treeType;
+    }
+
+    private TreeType find(int numberOfBranches, String color, boolean isExported) {
+        for (TreeType treeType : treeTypes) {
+            if (treeType.getNumberOfBranches() == numberOfBranches && treeType.getColor().equals(color) && treeType.isExported() == isExported)
+            return treeType;
+        }
+        return null;
+    }
+
+}
+
+class Forest
+{
+    private ArrayList<Tree> trees;
+
+    public void plantTree(int x, int y, int numberOfBranches, String color, boolean isExported) {
+        TreeType treeType = TreeFactory.getTreeType(numberOfBranches, color, isExported);
+        Tree tree = new Tree(x, y, treeType);
+        trees.add(tree);
+        System.out.println("Number of trees in the forest: " + trees.size());
+    }
+}
