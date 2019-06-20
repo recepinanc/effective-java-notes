feeb07ee9508123e374b3566240e1cd64d8def94
Recep İnanç <recepinanc@users.noreply.github.com>
2019-06-20 09:26:27 +0300

    Create Item3.java

---
+++ Creating and Destroying Objects/Item3.java
@@ -0,0 +1,39 @@
+package com.recepinanc.effectivejava.chapter2;
+
+public class Item3
+{
+    public static void main(String[] args)
+    {
+        int result = EnumSingleton.getInstance().getCounter();
+        System.out.println(result);
+    }
+}
+
+enum EnumSingleton
+{
+    INSTANCE;
+
+    // This constructor can be omitted completely
+    // since enum constructors are implicitly private
+    private EnumSingleton()
+    {
+
+    }
+
+    public int counter = 9;
+
+    public int getCounter()
+    {
+        return counter;
+    }
+
+    // You can also omit this method, but it 
+    // gives a better singleton feeling for the
+    // users of this class. Instead of accessing
+    // like: EnumSingleton.INSTANCE;
+    public static EnumSingleton getInstance()
+    {
+        return INSTANCE;
+    }
+}
+
